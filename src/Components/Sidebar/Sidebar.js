995dbf9e6103fc9204c3da5969da9fc66478e724
Your Name <samadhinilakshana1999@gmail.com>
2023-01-20 12:35:30 +0530

    Update Content

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,19 +1,33 @@
 import React from 'react'
 import logo from "../../assets/images/logo.png"
 import './Sidebar.css'
+import {UilEstate} from "@iconscout/react-unicons"
 
 const Sidebar = () => {
   return (
     <div className='Sidebar'>
       {/* logo */}
       <div className='logo'>
-        <img style={{width:'50px'}} src={logo} alt=''/>
+        <img style={{ width: '50px' }} src={logo} alt='' />
         <span>
           Sh<span>o</span>ps
         </span>
       </div>
 
+      {/* menu */}
+      <div className='menu'>
+        <div className='menuItem'>
+          <div>
+           <UilEstate/>
+          </div>
+          <span>Dashboard</span>
+
+        </div>
+      </div>
+
     </div>
+
+
   )
 }
 
