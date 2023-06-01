bc2eaeb52f34f1a4d5528fd0a25e1a1c1362bf1f
Samadhi Nilakshana <samadhi@commercialtp.com>
2023-06-01 12:03:15 +0530

    Complete Review Component

--- src/Components/RightSide/RightSide.js
+++ src/Components/RightSide/RightSide.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import './RightSide.css'
 import Updates from '../Updates/Updates'
+import Review from '../Review/Review'
 
 const RightSide = () => {
-  return (
-    <div className="RightSide">
-    <div>
-      <h3>Updates</h3>
-      <Updates/>
-    </div>
-    <div>
-      <h3>Customer Review</h3>
-      {/* <CustomerReview /> */}
-    </div>
-  </div>
-  )
+    return (
+        <div className="RightSide">
+            <div>
+                <h3>Updates</h3>
+                <Updates />
+            </div>
+            <div>
+                <h3>Customer Review</h3>
+                <Review />
+            </div>
+        </div>
+    )
 }
 
 export default RightSide
